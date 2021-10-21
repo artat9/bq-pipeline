19ef58d752fba76a522ea327ee6c409351e83464
atatur9 <momodarou2015@gmail.com>
2021-10-21 12:18:13 +0900

    refactor

--- lib/datasource.ts
+++ lib/datasource.ts
@@ -1,21 +1,8 @@
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
-import {
-  Domain,
-  DomainProps,
-  ElasticsearchVersion,
-} from "@aws-cdk/aws-elasticsearch";
-import {
-  AccountPrincipal,
-  Effect,
-  PolicyStatement,
-  ServicePrincipal,
-} from "@aws-cdk/aws-iam";
-import { LogGroup, RetentionDays } from "@aws-cdk/aws-logs";
 import * as cdk from "@aws-cdk/core";
 import * as environment from "./env";
 
 export class DataSourceStack extends cdk.Stack {
-  public readonly es: Domain;
   public readonly ddb: dynamodb.Table;
   constructor(
     scope: cdk.Construct,
@@ -24,8 +11,7 @@ export class DataSourceStack extends cdk.Stack {
   ) {
     super(scope, id);
     const tableName = environment.withEnvPrefix(target, "main");
-    //this.es = new Domain(this, "Domain", domainProps(this, target));
-    this.ddb = new dynamodb.Table(this, tableName, ddbProps(tableName, target));
+    this.ddb = new dynamodb.Table(this, tableName, ddbProps(tableName));
     withIndexes(this.ddb);
   }
 }
@@ -55,10 +41,7 @@ const withIndexes = (table: dynamodb.Table) => {
   });
 };
 
-const ddbProps = (
-  tableName: string,
-  target: environment.Environments
-): dynamodb.TableProps => {
+const ddbProps = (tableName: string): dynamodb.TableProps => {
   return {
     tableName: tableName,
     partitionKey: {
@@ -73,106 +56,4 @@ const ddbProps = (
     removalPolicy: cdk.RemovalPolicy.RETAIN,
     pointInTimeRecovery: true,
   };
-};
-
-const domainProps = (
-  scope: cdk.Construct,
-  target: environment.Environments
-): DomainProps => {
-  const domainName = environment.withEnvPrefix(target, "data");
-  const appLogGroup = esLogGroup(scope, target, "app");
-  const auditLogGroup = esLogGroup(scope, target, "audit");
-  const slowIndexLogGroup = esLogGroup(scope, target, "slowIndex");
-  const slowSearchLogGroup = esLogGroup(scope, target, "slowSearch");
-
-  return {
-    domainName: domainName,
-    version: ElasticsearchVersion.V7_9,
-    capacity: {
-      dataNodeInstanceType: esInstanceType(target),
-      masterNodes: 3,
-      masterNodeInstanceType: esInstanceType(target),
-      dataNodes: 3,
-    },
-    removalPolicy: cdk.RemovalPolicy.RETAIN,
-
-    zoneAwareness: {
-      availabilityZoneCount: 3,
-      enabled: true,
-    },
-    enforceHttps: true,
-    nodeToNodeEncryption: true,
-    encryptionAtRest: {
-      enabled: true,
-    },
-
-    accessPolicies: [
-      allowMyAccountToAccessEs(scope, domainName),
-      allowEsToWriteLogToCWLogs(scope, [
-        appLogGroup,
-        auditLogGroup,
-        slowIndexLogGroup,
-        slowSearchLogGroup,
-      ]),
-    ],
-
-    logging: {
-      appLogEnabled: true,
-      appLogGroup: appLogGroup,
-      //      auditLogEnabled: true,
-      //      auditLogGroup: auditLogGroup,
-      slowIndexLogEnabled: true,
-      slowIndexLogGroup: slowIndexLogGroup,
-      slowSearchLogEnabled: true,
-      slowSearchLogGroup: slowSearchLogGroup,
-    },
-    automatedSnapshotStartHour: 0,
-  };
-};
-
-const esInstanceType = (target: environment.Environments) => {
-  if (environment.isProd(target)) {
-    return "t3.medium.elasticsearch";
-  }
-  return "t3.small.elasticsearch";
-};
-
-const allowMyAccountToAccessEs = (
-  scope: cdk.Construct,
-  domainName: string
-): PolicyStatement => {
-  return new PolicyStatement({
-    effect: Effect.ALLOW,
-    actions: ["es:*"],
-    principals: [new AccountPrincipal(cdk.Stack.of(scope).account)],
-    resources: [
-      `arn:aws:es:${cdk.Stack.of(scope).region}:${
-        cdk.Stack.of(scope).account
-      }:domain/${domainName}/*`,
-    ],
-  });
-};
-
-const allowEsToWriteLogToCWLogs = (
-  scope: cdk.Construct,
-  logGroups: LogGroup[]
-): PolicyStatement => {
-  return new PolicyStatement({
-    effect: Effect.ALLOW,
-    actions: ["logs:PutLogEvents", "logs:CreateLogStream"],
-    principals: [new ServicePrincipal("es.amazonaws.com")],
-    resources: logGroups.map((lg) => lg.logGroupArn),
-  });
-};
-
-const esLogGroup = (
-  scope: cdk.Construct,
-  target: environment.Environments,
-  logName: string
-): LogGroup => {
-  return new LogGroup(scope, `es${logName}`, {
-    retention: RetentionDays.SIX_MONTHS,
-    removalPolicy: cdk.RemovalPolicy.DESTROY,
-    logGroupName: `/aws/es/${target.toString()}/${logName}`,
-  });
 };
