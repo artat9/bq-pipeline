f15194a3df3fbffb74f36250764a7b037e651a71
atatur9 <momodarou2015@gmail.com>
2021-10-22 17:00:43 +0900

    sign in

---
+++ lib/functions/lib/account/persistence/rep.go
@@ -0,0 +1,50 @@
+package accountrep
+
+import (
+	"context"
+	"kaleido-backend/lib/functions/lib/account"
+	"kaleido-backend/lib/functions/lib/common/log"
+	"kaleido-backend/lib/functions/lib/infrastructure/ddb"
+
+	"github.com/guregu/dynamo"
+)
+
+const (
+	pkPrefix = "Account:"
+	sk       = "Account:Account"
+)
+
+type (
+	// Repository repository.
+	Repository struct {
+		ddb *dynamo.DB
+	}
+
+	// DDBAccount account of ddb.
+	DDBAccount struct {
+		ddb.SimpleEntry
+	}
+)
+
+// New new repository
+func New(ddb *dynamo.DB) Repository {
+	return Repository{
+		ddb: ddb,
+	}
+}
+
+// New new account
+func (r Repository) New(ctx context.Context, ac account.Account) error {
+	e := fromAccount(ac)
+	if err := r.ddb.Table(ddb.Table()).Put(&e).RunWithContext(ctx); err != nil {
+		log.Error("put item failed", err)
+		return err
+	}
+	return nil
+}
+
+func fromAccount(ac account.Account) DDBAccount {
+	return DDBAccount{
+		SimpleEntry: ddb.NewSimpleEntry(pkPrefix+ac.Address.Hex(), sk, ac.Nonce.String()),
+	}
+}
