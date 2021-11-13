bcbc2aa8dce51d44a490ad7b510f5403a3b21fa8
atatur9 <momodarou2015@gmail.com>
2021-11-13 17:23:01 +0900

    upload media info via cli

---
+++ cmd/newmedia/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	ipfsclient "kaleido-backend/pkg/infrastructure/ipfs"
+	"kaleido-backend/pkg/mediaaccount"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+var (
+	notempty = func(input string) bool {
+		return input != ""
+	}
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	input := mediaaccount.Application{}
+	add := text(scanner, "address of account", func(input string) bool {
+		eoa := common.HexToAddress(input)
+		return eoa != common.Address{}
+	})
+	input.Account = common.HexToAddress(add)
+	input.Name = text(scanner, "media name", notempty)
+	input.Description = text(scanner, "description", notempty)
+	input.URL = text(scanner, "url", notempty)
+	b, _ := json.Marshal(&input.PublicInfo)
+	fmt.Println("uploading json")
+	fmt.Println(string(b))
+	cid, err := ipfsclient.NewIpfsClient().UploadMedia(context.Background(), input.PublicInfo)
+	if err != nil {
+		fmt.Println("unexpected error occured")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("added " + cid)
+}
+
+func text(sc *bufio.Scanner, inputDescription string, matchFunc func(input string) bool) string {
+	fmt.Println(inputDescription)
+	for sc.Scan() {
+		input := sc.Text()
+		if matchFunc(input) {
+			return input
+		}
+		fmt.Println("enter " + inputDescription + " again.")
+	}
+	return ""
+}
